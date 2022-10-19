13ea01a52e251942f0ab93b59c52596d85659a8c
minhaaz717 <86486466+minhaaz717@users.noreply.github.com>
2022-10-18 20:08:09 -0500

    Update numpy.py

--- numpy.py
+++ numpy.py
@@ -9,3 +9,15 @@ for i in a:
     i[np.where(i==i.max())] = 0
 print("maximum value replaced by 0:")
 print(a)
+
+OUTPUT: 
+array:
+[ 1 16 12  3 10  1  1 10 15 10 13 10  9 19 10]
+[[ 1 16 12  3 10]
+ [ 1  1 10 15 10]
+ [13 10  9 19 10]]
+(3, 5)
+maximum value replaced by 0:
+[[ 1  0 12  3 10]
+ [ 1  1 10  0 10]
+ [13 10  9  0 10]]    
